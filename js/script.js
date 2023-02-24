630d5663cd12ba1f05a24897cd01587140cd7613
Rafael de Lima <rafaelftdelima@gmail.com>
2023-02-24 16:54:02 -0300

    Implements script for smooth scroll

--- js/script.js
+++ js/script.js
@@ -36,5 +36,34 @@ function initAccordionList() {
     }
 }
 
+function initSmoothScroll() {
+    const internalLinks = document.querySelectorAll('.js-navegation a[href^="#"]');
+
+    if (internalLinks.length) {
+        function scrollToSection(event) {
+            event.preventDefault();
+
+            const href = event.currentTarget.getAttribute("href");
+            const section = document.querySelector(href);
+            /* const top = section.offsetTop;
+                
+                window.scrollTo({
+                top: top,
+                behavior: "smooth"
+            }); */
+
+            section.scrollIntoView({
+                behavior: "smooth",
+                block: "start"
+            });
+        }
+
+        internalLinks.forEach(link => {
+            link.addEventListener("click", scrollToSection);
+        });
+    }
+}
+
 initTabNavegation();
 initAccordionList();
+initSmoothScroll();

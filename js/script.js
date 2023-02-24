920fa8991c82eb4c49f6e0841fda48efa8452246
Rafael de Lima <rafaelftdelima@gmail.com>
2023-02-24 15:39:00 -0300

    Implements script for FAQ's slide down manipulation

--- js/script.js
+++ js/script.js
@@ -1,18 +1,40 @@
-const tabMenu = document.querySelectorAll(".js-tabmenu li");
-const tabContent = document.querySelectorAll(".js-tabcontent section");
+function initTabNavegation() {
+    const tabMenu = document.querySelectorAll(".js-tabmenu li");
+    const tabContent = document.querySelectorAll(".js-tabcontent section");
 
-if (tabMenu.length && tabContent.length) {
-    tabContent[0].classList.add("actived");
+    if (tabMenu.length && tabContent.length) {
+        tabContent[0].classList.add("actived");
 
-    function activeTab(index) {
-        tabContent.forEach(section => {
-            section.classList.remove("actived");
+        function activeTab(index) {
+            tabContent.forEach(section => {
+                section.classList.remove("actived");
+            });
+
+            tabContent[index].classList.add("actived");
+        }
+
+        tabMenu.forEach((item, index) => {
+            item.addEventListener("click", () => activeTab(index));
         });
-
-        tabContent[index].classList.add("actived");
     }
-
-    tabMenu.forEach((item, index) => {
-        item.addEventListener("click", () => activeTab(index));
-    });
 }
+
+function initAccordionList() {
+    const accordionList = document.querySelectorAll(".js-accordion dt");
+
+    if (accordionList.length) {
+        accordionList[0].classList.add("actived");
+
+        function activeAccordion(event) {
+            console.log(event.currentTarget);
+            event.currentTarget.classList.toggle("actived");
+        }
+
+        accordionList.forEach(item => {
+            item.addEventListener("click", activeAccordion);
+        });
+    }
+}
+
+initTabNavegation();
+initAccordionList();

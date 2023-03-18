14511bddf7940792fa81c7f4d23be53cfb48695d
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-18 09:51:30 -0300

    Export initScrollAnimation function

---
+++ js/modules/animate-scroll.js
@@ -0,0 +1,23 @@
+export default function initScrollAnimation() {
+    const sectionsList = document.querySelectorAll('[data-page="section"]');
+
+    if (sectionsList.length) {
+        const animationPoint = window.innerHeight * 0.6;
+
+        function animateScroll() {
+            sectionsList.forEach(section => {
+                const sectionTopDistance = section.getBoundingClientRect().top - animationPoint;
+
+                if (sectionTopDistance < 0) {
+                    section.classList.add("actived");
+                }
+                /* else {
+                    section.classList.remove("actived");
+                } */
+            })
+        }
+
+        animateScroll();
+        window.addEventListener("scroll", animateScroll);
+    }
+}

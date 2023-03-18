1113562304e9b45748e2c2b7c49b7b3cb5edada7
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-18 09:50:08 -0300

    Import all animation modules and export initAnimations function

---
+++ js/modules/animations.js
@@ -0,0 +1,11 @@
+import initAccordionList from './accordion-list.js';
+import initScrollAnimation from './animate-scroll.js';
+import initSmoothScroll from './smooth-scroll.js';
+import initTabNavegation from './tab-navegation.js';
+
+export default function initAnimations() {
+    initAccordionList();
+    initScrollAnimation();
+    initSmoothScroll();
+    initTabNavegation();
+}

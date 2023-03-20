6ee92d4fe3009a05009b5d0b05625a6eeb032ac3
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-20 19:43:22 -0300

    Add outsideClick functionality

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,10 +1,39 @@
 export default function initDropdown() {
     const dropdowns = document.querySelectorAll('[data-dropdown]');
 
+    function outsideClick(element, events, callback) {
+        const html = document.documentElement;
+        const outside = 'data-outside';
+
+        function handleOutsideClick(event) {
+            if (!element.contains(event.target)) {
+                element.removeAttribute(outside);
+
+                events.forEach(event => {
+                    html.removeEventListener(event, handleOutsideClick);
+                });
+
+                callback();
+            }
+        }
+
+        if (!element.hasAttribute(outside)) {
+            events.forEach(event => {
+                html.addEventListener(event, handleOutsideClick);
+            });
+
+            element.setAttribute(outside, '');
+        }
+    }
+
     function handleClick(event) {
         event.preventDefault();
 
         this.classList.toggle('actived');
+
+        outsideClick(this, ['click', 'touchstart'], () => {
+            this.classList.remove('actived');
+        });
     }
 
     dropdowns.forEach(dropdown => {

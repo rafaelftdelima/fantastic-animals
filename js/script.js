9ddf78982b4e300eafb459a2a7d6d2f3ee895088
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-20 17:57:04 -0300

    Import dropdown file

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 import initAnimations from "./modules/animations.js";
+import initDropdown from "./modules/dropdown.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 
 initAnimations();
 initModal();
 initTooltip();
+initDropdown();

f391f781ad1a9ae591837bee15a824e038cfef9d
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-20 19:14:07 -0300

    Implements script for dropdown functionality

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,3 +1,15 @@
 export default function initDropdown() {
+    const dropdowns = document.querySelectorAll('[data-dropdown]');
 
+    function handleClick(event) {
+        event.preventDefault();
+
+        this.classList.toggle('actived');
+    }
+
+    dropdowns.forEach(dropdown => {
+        ['click', 'touchstart'].forEach(event =>
+            dropdown.addEventListener(event, handleClick)
+        );
+    });
 }

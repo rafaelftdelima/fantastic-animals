599d021a9e874861bfb5309c67cc282eaaf49a9d
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-20 15:47:23 -0300

    Import initTooltip function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initAnimations from "./modules/animations.js";
-import initModal from "./modules/modal.js"
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tooltip.js";
 
 initAnimations();
 initModal();
+initTooltip();

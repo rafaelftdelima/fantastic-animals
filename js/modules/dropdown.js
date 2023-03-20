7a4e5064b2a297d9c79cb142e0dd13355cdb7186
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-20 19:49:05 -0300

    Remove outsideClick function of module implementation

--- js/modules/dropdown.js
+++ js/modules/dropdown.js
@@ -1,30 +1,7 @@
+import outsideClick from "./outside-click.js";
+
 export default function initDropdown() {
     const dropdowns = document.querySelectorAll('[data-dropdown]');
-
-    function outsideClick(element, events, callback) {
-        const html = document.documentElement;
-        const outside = 'data-outside';
-
-        function handleOutsideClick(event) {
-            if (!element.contains(event.target)) {
-                element.removeAttribute(outside);
-
-                events.forEach(event => {
-                    html.removeEventListener(event, handleOutsideClick);
-                });
-
-                callback();
-            }
-        }
-
-        if (!element.hasAttribute(outside)) {
-            events.forEach(event => {
-                html.addEventListener(event, handleOutsideClick);
-            });
-
-            element.setAttribute(outside, '');
-        }
-    }
 
     function handleClick(event) {
         event.preventDefault();

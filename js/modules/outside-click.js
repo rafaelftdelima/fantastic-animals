80226dd2311887be9b96f07ceabdee40427f9445
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-20 19:48:09 -0300

    File for outsideClick function

---
+++ js/modules/outside-click.js
@@ -0,0 +1,24 @@
+export default function outsideClick(element, events, callback) {
+    const html = document.documentElement;
+    const outside = 'data-outside';
+
+    function handleOutsideClick(event) {
+        if (!element.contains(event.target)) {
+            element.removeAttribute(outside);
+
+            events.forEach(event => {
+                html.removeEventListener(event, handleOutsideClick);
+            });
+
+            callback();
+        }
+    }
+
+    if (!element.hasAttribute(outside)) {
+        events.forEach(event => {
+            html.addEventListener(event, handleOutsideClick);
+        });
+
+        element.setAttribute(outside, '');
+    }
+}

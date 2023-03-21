d3fbe35fee6d0083bda8f10db2015f63c07e89ac
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-21 18:49:38 -0300

    Add numbers' script file

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initDropdown from "./modules/dropdown.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initMenuMobile from "./modules/menu-mobile.js";
+import initNumbers from "./modules/numbers.js";
 
 initAnimations();
 initModal();
 initTooltip();
 initDropdown();
 initMenuMobile();
+initNumbers();

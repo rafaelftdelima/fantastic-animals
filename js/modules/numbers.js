6019de36acfff6266a4e7cc59703fd962f910374
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-21 19:08:44 -0300

    Implement script for numbers section' animation

--- js/modules/numbers.js
+++ js/modules/numbers.js
@@ -1,3 +1,23 @@
 export default function initNumbers() {
+    const numbers = document.querySelectorAll('[data-number]');
 
+    numbers.forEach(number => {
+        const total = +number.innerText;
+        const increment =   Math.round(total / 25);
+        let start = 0;
+
+        number.innerText = '0';
+
+        const time = setInterval(() => {
+            number.innerText = start;
+
+            start += increment;
+
+            if (start > total) {
+                clearInterval(time);
+
+                number.innerText = total;
+            }
+        }, 50);
+    });
 }

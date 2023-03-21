a1cf4d0861bbd28c26a08eadd24a123e4714f805
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-21 18:19:33 -0300

    Import menu mobile' script

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initAnimations from "./modules/animations.js";
 import initDropdown from "./modules/dropdown.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initMenuMobile from "./modules/menu-mobile.js";
 
 initAnimations();
 initModal();
 initTooltip();
 initDropdown();
+initMenuMobile();

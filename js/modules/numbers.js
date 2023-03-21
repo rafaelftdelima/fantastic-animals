3acff9763c1a55fb4dd048851cc62ab3f08c43a4
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-21 19:27:17 -0300

    Add observer for numbers animation - bug correction

--- js/modules/numbers.js
+++ js/modules/numbers.js
@@ -1,23 +1,40 @@
 export default function initNumbers() {
-    const numbers = document.querySelectorAll('[data-number]');
+    function animateNumbers() {
+        const numbers = document.querySelectorAll('[data-number]');
 
-    numbers.forEach(number => {
-        const total = +number.innerText;
-        const increment =   Math.round(total / 25);
-        let start = 0;
+        numbers.forEach(number => {
+            const total = +number.innerText;
+            const increment = Math.round(total / 25);
+            let start = 0;
 
-        number.innerText = '0';
+            number.innerText = '0';
 
-        const time = setInterval(() => {
-            number.innerText = start;
+            const timer = setInterval(() => {
+                number.innerText = start;
 
-            start += increment;
+                start += increment;
 
-            if (start > total) {
-                clearInterval(time);
+                if (start > total) {
+                    clearInterval(timer);
 
-                number.innerText = total;
-            }
-        }, 50);
+                    number.innerText = total;
+                }
+            }, 50);
+        });
+    }
+
+    const target = document.querySelector('.numbers');
+    const observer = new MutationObserver(handleMutation);
+
+    function handleMutation(mutation) {
+        if (mutation[0].target.classList.contains('actived')) {
+            observer.disconnect();
+
+            animateNumbers();
+        }
+    }
+
+    observer.observe(target, {
+        attributes: true
     });
 }

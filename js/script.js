f86f243e8b17207e369eb0fa8b4c0712a9a27f21
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-03-22 09:54:21 -0300

    Import store status' script file

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initNumbers from "./modules/numbers.js";
+import initStoreStatus from "./modules/store-status.js";
 
 initAnimations();
 initModal();
@@ -11,3 +12,4 @@ initTooltip();
 initDropdown();
 initMenuMobile();
 initNumbers();
+initStoreStatus();

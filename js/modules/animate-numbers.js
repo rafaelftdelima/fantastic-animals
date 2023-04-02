5d7dd6d9faf45e8463b9091fb759522ab03cd4d2
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-04-02 18:42:35 -0300

    Separate numbers animation' function and API fetch simulation

---
+++ js/modules/animate-numbers.js
@@ -0,0 +1,40 @@
+export default function initAnimateNumbers() {
+    function animateNumbers() {
+        const numbers = document.querySelectorAll('[data-number]');
+
+        numbers.forEach(number => {
+            const total = +number.innerText;
+            const increment = Math.round(total / 25);
+            let start = 0;
+
+            number.innerText = '0';
+
+            const timer = setInterval(() => {
+                number.innerText = start;
+
+                start += increment;
+
+                if (start > total) {
+                    clearInterval(timer);
+
+                    number.innerText = total;
+                }
+            }, 50);
+        });
+    }
+
+    const target = document.querySelector('.numbers');
+    const observer = new MutationObserver(handleMutation);
+
+    function handleMutation(mutation) {
+        if (mutation[0].target.classList.contains('actived')) {
+            observer.disconnect();
+
+            animateNumbers();
+        }
+    }
+
+    observer.observe(target, {
+        attributes: true
+    });
+}

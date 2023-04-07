39e3caa0d0c4e49ffddbf03e4c4a6da5b758968e
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-04-07 16:55:46 -0300

    file refactorated

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
-import initAnimations from "./modules/animations.js";
-import initDropdown from "./modules/dropdown.js";
-import initModal from "./modules/modal.js";
-import initTooltip from "./modules/tooltip.js";
-import initMenuMobile from "./modules/menu-mobile.js";
-import initNumbers from "./modules/numbers.js";
-import initStoreStatus from "./modules/store-status.js";
-import initFetchBitcoin from "./modules/fetch-bitcoin.js";
+import fetchAnimals from './modules/fetch-animals.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
 
-initAnimations();
-initModal();
-initTooltip();
-initDropdown();
-initMenuMobile();
-initNumbers();
-initStoreStatus();
-initFetchBitcoin();
+import Accordion from './modules/accordion.js';
+import DropdownMenu from './modules/dropdown-menu.js';
+import MobileMenu from './modules/mobile-menu.js';
+import Modal from './modules/modal.js';
+import ScrollAnimation from './modules/scroll-animation.js';
+import SmoothScroll from './modules/smooth-scroll.js';
+import StoreStatus from './modules/store-status.js';
+import TabNav from './modules/tabnav.js';
+import Tooltip from './modules/tooltip.js';
+
+const accordion = new Accordion('[data-page="accordion"] dt');
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+const mobileMenu = new MobileMenu('[data-menu="button"]', '[data-menu="list"]');
+const modal = new Modal(
+    '[data-modal="open"]',
+    '[data-modal="close"]',
+    '[data-modal="container"]'
+);
+const scrollAnimation = new ScrollAnimation('[data-page="section"]');
+const smoothScroll = new SmoothScroll('[data-page="navegation"] a[href^="#"]');
+const storeStatus = new StoreStatus('[data-status]', 'actived');
+const tabNav = new TabNav(
+    '[data-page="menu"] li',
+    '[data-tab="content"] section'
+);
+const tooltip = new Tooltip('[data-tooltip]');
+
+accordion.init();
+dropdownMenu.init();
+mobileMenu.init();
+modal.init();
+scrollAnimation.init();
+smoothScroll.init();
+storeStatus.init();
+tabNav.init();
+tooltip.init();
+
+fetchAnimals('../../animals-api.json', '.numbers-grid');
+fetchBitcoin('https://blockchain.info/ticker', '[data-bitcoin]');

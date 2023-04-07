6872e83716fd39e4b22c85cb7d5745a5fddb5ddf
Rafael de Lima <rafaelftdelima@edu.unirio.br>
2023-04-07 15:08:55 -0300

    File refactorated

---
+++ js/modules/mobile-menu.js
@@ -0,0 +1,41 @@
+import outsideClick from './outsideclick.js';
+
+export default class MobileMenu {
+    constructor(button, menuList, events) {
+        this.button = document.querySelector(button);
+        this.menuList = document.querySelector(menuList);
+        this.activeClass = 'actived';
+
+        if (events === undefined) {
+            this.events = ['touchstart', 'click'];
+        } else {
+            this.events = events;
+        }
+
+        this.openMenu = this.openMenu.bind(this);
+    }
+
+    openMenu() {
+        this.button.classList.add(this.activeClass);
+        this.menuList.classList.add(this.activeClass);
+
+        outsideClick(this.menuList, this.events, () => {
+            this.button.classList.remove(this.activeClass);
+            this.menuList.classList.remove(this.activeClass);
+        });
+    }
+
+    addMobileMenuEvent() {
+        this.events.forEach((event) => {
+            this.button.addEventListener(event, this.openMenu);
+        });
+    }
+
+    init() {
+        if (this.button && this.menuList) {
+            this.addMobileMenuEvent();
+        }
+
+        return this;
+    }
+}
